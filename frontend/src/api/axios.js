75061ad244b87669d96423a470b39b23c03b74a5
Esther Ani <estherani10000@gmail.com>
2026-07-17 14:50:02 +0100

    update commit

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -5,7 +5,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: '/api',
+  baseURL: 'https://blaisetech-platform-production.up.railway.app/',
 });
 
 api.interceptors.request.use((config) => {
