37fa03eb9b0da21a32aae965585657217d54b302
Esther Ani <estherani10000@gmail.com>
2026-07-17 12:39:28 +0100

    update commit

--- backend/server.js
+++ backend/server.js
@@ -30,7 +30,9 @@ const messageRoutes = require('./routes/messageRoutes');
 const app = express();
 
 // --- Global middleware ---
-app.use(cors());
+app.use(cors({
+  origin: 'https://blaisetechglobalresources.netlify.app',
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/uploads', express.static(path.join(__dirname, 'uploads'))); // serve uploaded photos/CVs
